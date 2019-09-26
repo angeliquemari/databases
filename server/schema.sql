d51a7fad20443df9f07a26a9ad83920814958a29
angeliquemari <angelique@nehmzow.com>
2019-09-25 17:54:04 -0700

    yay! friends!!

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,49 @@
-CREATE DATABASE chat;
+-- drop database chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+drop table if exists messages;
+drop table if exists friends;
+drop table if exists users;
+drop table if exists rooms;
+
+create table users (
+  id int auto_increment primary key,
+  username varchar(20) unique
 );
 
-/* Create other tables and define schemas for them here! */
+create table rooms (
+  id int auto_increment primary key,
+  roomname varchar(20) unique
+);
 
+create table messages (
+  id int auto_increment primary key,
+  message varchar(280),
+  user_id int not null,
+  room_id int not null,
+  created_at timestamp default current_timestamp,
+
+  foreign key(user_id)
+    references users (id),
+
+  foreign key(room_id)
+    references rooms (id)
+);
+
+create table friends (
+  id int auto_increment primary key,
+  friender_user_id int not null,
+  friendee_user_id int not null,
+
+  foreign key(friender_user_id)
+    references users (id),
+
+  foreign key(friendee_user_id)
+    references users (id)
+);
 
 
 
